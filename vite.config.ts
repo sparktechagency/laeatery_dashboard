fb022f09eedde3ed079dcaa922f2df66e8d6c48b
goni715 <halesgoni715149123@gmail.com>
2025-06-19 15:34:44 +0600

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
  
- 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',  
-    port: 3001       
+    host: '0.0.0.0',
+    port: 3001
   },
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+    allowedHosts: ['dashboard.laeatery.com']
+  }
 })
